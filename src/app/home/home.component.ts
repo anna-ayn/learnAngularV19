6f64e40f27cf88c2709a84fbb4fdfe4599af4a87
anna-ayn <19-10096@usb.ve>
2025-02-04 16:23:01 -0400

    Refactor housing service to use async/await for data fetching and update details component to handle promises

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -36,7 +36,12 @@ export class HomeComponent {
   filteredLocationList: HousingLocation[] = [];
 
   constructor() {
-    this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.housingService
+      .getAllHousingLocations()
+      .then((housingLocationList: HousingLocation[]) => {
+        this.housingLocationList = housingLocationList;
+        this.filteredLocationList = housingLocationList;
+      });
   }
 
   filterResults(text: string) {
